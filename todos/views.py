f75ff8d6b8bc1422c8098c0feb866e6a64b581ee
Taraldinn <kferdoush617@gmail.com>
2023-03-04 01:10:26 +0600

    initial commit

---
+++ todos/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework import generics
+from .models import Todo
+
+from .serializers import TodoSerializers
+
+class ListTodo(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers
+    
+class DetailTodo(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializers
+    
